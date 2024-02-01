89da074124bc4e8e0b3e40ea74c978977e95934e
d.ketov <danielketovone@gmail.com>
2024-02-01 15:16:12 +0500

    change icons

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -8,14 +8,14 @@ export const Footer = () => {
 				<img className='footer-logo' src={assets.footerLogo} alt="logo" width='100' height='130' />
 				<div className='media-bg__box'>
 					<div className="social-media">
-						<a href="#!">
+						<a href="https://twitter.com/AmoreOnEth" target='_blank' rel="noreferrer">
+							<img src={assets.twitter} alt="twitter" />
+						</a>
+						<a href="https://t.me/havavaba" target='_blank' rel="noreferrer">
 							<img src={assets.telgram} alt="telegram" />
 						</a>
-						<a href="#!">
+						<a href="https://t.me/AmoreOnEth" target='_blank' rel="noreferrer">
 							<img src={assets.bestIcon} alt="best icon" />
-						</a>
-						<a href="#!">
-							<img src={assets.twitter} alt="twitter" />
 						</a>
 					</div>
 					<nav>
